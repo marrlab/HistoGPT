f856ec2e74272b04c4e1db5ae15e4282f4ec83a9
Manuel Tran <95863934+manuel-tran@users.noreply.github.com>
2024-03-14 23:57:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version="0.1.0",
     description="HistoGPT - PyTorch",
     packages=find_packages(exclude=[]),
-    #install_requires=[
-    #    #"torch>=2.1.0",
-    #    #"transformers>=4.37.2",
-    #    #"flamingo-pytorch>=0.1.2",
-    #],
+    install_requires=[
+        #"torch>=2.1.0",
+        #"transformers>=4.37.2",
+        "flamingo-pytorch>=0.1.2",
+    ],
 )

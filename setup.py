23ddbde5125c94e0f9ad35a7139aa1695f7dace8
Manuel Tran <95863934+manuel-tran@users.noreply.github.com>
2024-03-15 01:41:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,11 @@ setup(
     install_requires=[
         "einops>=0.4",
         "einops-exts",
+        "sacremoses>=0.1.1",
+        "slideio>=2.5.0"
+        "timm>=0.9.16"
         "torch>=2.1.0",
         "transformers>=4.37.2",
-        "sacremoses>=0.1.1",
         #"flamingo-pytorch>=0.1.2",
     ],
 )
